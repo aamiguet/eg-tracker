fd2c5ddba79a4bf110f68b1a67349e060f958ee3
Antoine Amiguet <amiguet.antoine@gmail.com>
2020-01-15 21:45:53 +0100

    Ajout de la route en couleur

--- main.js
+++ main.js
@@ -7,6 +7,19 @@ const parser = new xml2js.Parser({ attrkey: "ATTR" });
 const url = 'https://groups.google.com/forum/feed/spectrum-hby5404-tracking/msgs/rss_v2_0.xml?num=100';
 const itemTitle = 'SMS from 881631...@msg.iridium.com';
 
+const stHelenaMarker = {
+  type: "Feature",
+  properties: {},
+  geometry: {
+    type: "Point",
+    coordinates: [
+      -5.785443,
+      -15.9587411
+    ]
+  }
+};
+const strokeColors = ['#ff0000', '#00ff00'];
+
 function coordinate(item) {
   const descr = item.description[0];
   const lat = Number(descr.match(/Lat[^\s]+/)[0].substr(3));
@@ -14,31 +27,49 @@ function coordinate(item) {
   return [lon, lat];
 }
 
+function line(coordinates, properties) {
+  return {
+    type: 'Feature',
+    properties: properties,
+    geometry: {
+      type: 'LineString',
+      coordinates: coordinates
+    }
+  };
+}
+
 function geo(coordinates) {
   return {
     type: 'FeatureCollection',
     features: [
-      {
-        type: 'Feature',
-        properties: {},
-        geometry: {
-          type: 'LineString',
-          coordinates: coordinates
-        }
-      },
-      {
-        type: "Feature",
-        properties: {},
-        geometry: {
-          type: "Point",
-          coordinates: [
-            -5.785443,
-            -15.9587411
-          ]
-        }
-      }
+      line(coordinates, {}),
+      stHelenaMarker
     ]
   };
+}
+
+function coloredGeo(mappedItems) {
+  let features = [];
+  let lastItem = [];
+  mappedItems.forEach((items, date) => {
+    const dateItems = lastItem.concat(items);
+    const coords = _.map(dateItems, coordinate);
+    const colorIndex = new Date(date).getDate() % 2;
+    const stroke = strokeColors[colorIndex];
+    const props = {
+      stroke: stroke,
+      "stroke-width": 5,
+      "stroke-opacity": 0.5,
+      date: date
+    }
+    features.push(line(coords, props))
+    lastItem = [items[items.length - 1]];
+  });
+  features.push(stHelenaMarker);
+  return { 
+    type: 'FeatureCollection',
+    features: features
+  };  
 }
 
 function matchItem(storedItems, itemTest) {
@@ -71,6 +102,16 @@ function sortItems(item1, item2) {
   return d1 - d2;
 }
 
+function itemsByDate(items) {
+  let itemsMap = new Map();
+  items.forEach(item => {
+    const pubDate = new Date(item.pubDate[0]).toDateString();
+    if (!itemsMap.has(pubDate)) itemsMap.set(pubDate, []);
+    itemsMap.get(pubDate).push(item);
+  });
+  return itemsMap;
+}
+
 function process(xml) {
   parser.parseString(xml, (error, result) => {
     if (error === null) {
@@ -78,6 +119,7 @@ function process(xml) {
       const allItems = updateItems(items).sort(sortItems);
       const coords = _.map(allItems, coordinate);
       fs.writeFileSync('eg-route.json', JSON.stringify(geo(coords), null, 2));
+      fs.writeFileSync('eg-colored-route.json', JSON.stringify(coloredGeo(itemsByDate(allItems)), null, 2));
     } else {
       console.log(error);
     }
